9edf5dfa72fd1679a82efc414d5f5c9ded942cd2
Minttokae <140225926+Minttokae@users.noreply.github.com>
2025-02-04 01:19:08 +0900

    Create 1_1.cpp

---
+++ Ch01/1_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    int i, sum = 0;
+    for (int j = 1; j <= 5; j++){
+        std::cout<<j<<"번째 정수입력 :";
+        std::cin>>i;
+        std::endl;
+        sum += i;
+    }
+    std::cout<<"합계 :"<<sum;
+    return 0;
+}
