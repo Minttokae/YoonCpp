178b1edd5b158353b4a6df4d81cd95085634a1c7
Minttokae <140225926+Minttokae@users.noreply.github.com>
2025-02-04 01:27:48 +0900

    Create 1_2.cpp

---
+++ Ch01/1_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char name[20], num[20];
+    
+    cin>>name;
+    cin>>num;
+    
+    cout<<"이름 : "<<name<<endl;
+    cout<<"전화번호 : "<<num;
+    
+    return 0;
+}
