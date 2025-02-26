6731b4b12771cd398c8b81bc3cacf78111cf8812
Minttokae <140225926+Minttokae@users.noreply.github.com>
2025-02-27 01:23:20 +0900

    Create accountMain.cpp

---
+++ 단계별 프로젝트/2단계/accountMain.cpp
@@ -0,0 +1,27 @@
+#include "header.h"
+
+
+int main() {
+    int select;
+
+    while (1) {
+        Mainpage();
+
+        cin >> select;
+        cout << endl;
+
+        switch (select)
+        {
+        case 1: CreateAccount(); break;
+        case 2: Deposit();  break;
+        case 3: Withdraw();  break;
+        case 4: PrintAccounts(); break;
+        case 5: Deleteall(); exit(0);
+
+        default: 
+            cout << "잘못된 값" << endl << endl; continue;
+        }
+    }
+
+    return 0;
+}
