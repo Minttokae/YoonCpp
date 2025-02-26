45523e3a19da2e78a285f9fd57d98556185234c7
Minttokae <140225926+Minttokae@users.noreply.github.com>
2025-02-27 01:23:53 +0900

    Create header.h

---
+++ 단계별 프로젝트/2단계/header.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+#include <cstdlib> //exit
+#pragma warning(disable:4996)
+#include <conio.h> // getch
+#include <cstring>
+
+#define MAX_CLIENT 50
+#define NAME_LEN 10
+
+using namespace std;
+
+
+void Mainpage();
+
+class Account {
+	int ID;
+	char* name;
+	int myMoney;
+public:
+	Account(int ID, char name[NAME_LEN], int balance);
+	int GetID();
+	char* Getname();
+	int Getmoney();
+	void DepWith(int money, int type);
+	~Account();
+};
+
+int FindID(int ID);
+bool CreateAccount();
+void Deposit();
+void Withdraw();
+void PrintAccounts();
+void Deleteall();
