ffc97ef5ae47010d9d6b29ca2e2b0e5e27fe0974
Minttokae <140225926+Minttokae@users.noreply.github.com>
2025-02-27 12:16:37 +0900

    Create header.cpp

---
+++ 단계별 프로젝트/3단계/header.cpp
@@ -0,0 +1,137 @@
+#include "header.h"
+
+int top = 0;
+Account* Accounts[MAX_CLIENT];
+
+void Mainpage() {
+    cout << "-----MENU-----" << endl;
+    cout << "1. 계좌개설" << endl;
+    cout << "2. 입    금" << endl;
+    cout << "3. 출    금" << endl;
+    cout << "4. 전체고객 잔액조회" << endl;
+    cout << "5. 프로그램 종료" << endl << endl;
+
+    cout << "선택: ";
+}
+
+Account::Account(int ID, char inname[NAME_LEN], int balance) : ID(ID), myMoney(balance)
+{
+    name = new char[strlen(inname) + 1];
+    strcpy(name, inname);
+};
+
+Account::Account(const Account& copy) : ID(copy.ID), myMoney(copy.myMoney) {
+    name = new char[strlen(copy.name) + 1];
+    strcpy(name, copy.name);
+}
+
+int Account::GetID() {
+    return ID;
+}
+char* Account::Getname() {
+    return name;
+}
+int Account::Getmoney() {
+    return myMoney;
+}
+void Account::DepWith(int money, int type) {
+    if (type == 0)
+        myMoney += money;
+    else if (type == 1)
+        if (myMoney > money)
+            myMoney -= money;
+        else {
+            cout << "잔액 부족";
+            return;
+        }
+}
+
+Account::~Account() {
+    delete[]name;
+}
+int FindID(int ID) {
+    int i;
+    for (i = 0; i <= top; i++) {
+        if (ID == Accounts[i]->GetID())
+            return i;
+    }
+    return -1;
+}
+
+bool CreateAccount() {
+    int MakeID, money;
+    char cname[NAME_LEN];
+
+    cout << "ID를 입력해주세요: ";
+    cin >> MakeID;
+    cout << "이름을 입력해주세요: ";
+    cin >> cname;
+    cout << "입금할 금액을 입력해주세요: ";
+    cin >> money;
+
+    Accounts[top] = new Account(MakeID, cname, money);
+    top++;
+
+    return 1;
+}
+
+void Deposit() {
+    int ID, money;
+    int i;
+    cout << "ID를 입력해주세요: ";
+    cin >> ID;
+
+    i = FindID(ID);
+
+    if (i == -1) {
+        cout << "올바르지 않은 아이디 입니다." << endl;
+        return;
+    }
+
+    cout << Accounts[i]->Getname() << " 고객님 " << "입금하실 금액을 입력해주세요:";
+    cin >> money;
+    Accounts[i]->DepWith(money, 0);
+
+    cout << "입금 후 잔액: " << Accounts[i]->Getmoney() << endl;
+    cout << "아무 키를 누르면 메인화면으로 이동합니다." << endl<<endl;
+
+    _getch();
+}
+
+void Withdraw() {
+    int ID, money;
+    int i;
+    cout << "ID를 입력해주세요: ";
+    cin >> ID;
+
+    i = FindID(ID);
+
+    if (i == -1) {
+        cout << "올바르지 않은 아이디 입니다." << endl;
+        return;
+    }
+
+    cout << Accounts[i]->Getname() << "고객님 " << "출금하실 금액을 입력해주세요:";
+    cin >> money;
+    Accounts[i]->DepWith(money, 1);
+
+    cout << "출금 후 잔액: " << Accounts[i]->Getmoney() << endl;
+    cout << "아무 키를 누르면 메인화면으로 이동합니다." << endl << endl;
+
+    _getch();
+}
+
+void PrintAccounts() {
+    for (int i = 0; i < top; i++) {
+        cout << "ID: " << Accounts[i]->GetID() << endl;
+        cout << "이름: " << Accounts[i]->Getname() << endl;
+        cout << "잔액: " << Accounts[i]->Getmoney() << endl << endl;
+    }
+}
+
+void Deleteall() {
+    for (int i = 0; i < top; i++) {
+        delete Accounts[i];
+    }
+}
+
